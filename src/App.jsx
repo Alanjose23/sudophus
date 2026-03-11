c167638c7ac3d88d524e0f135088bb21a2ada1d1
AlanJ <alan.jose123@icloud.com>
2026-03-11 13:13:28 -0400

    created basic UI for application

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -8,26 +7,19 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">''
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div><center>
+        <img src="../public/sudo.jpg" alt="climb the mountain" />
+      <h1>Sudophus</h1></center>
+      <h4>Welcome to Sudophus,a simple React Journaling Application which documents progress of your coding journey</h4>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          log entries {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Click this button to add a journal entry
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
